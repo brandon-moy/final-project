378d610672845d15882f2b866d3febf82f48ade6
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-11-04 23:51:48 +0000

    Set up database schema and tested with dummy account inserted into table

---
+++ database/data.sql
@@ -0,0 +1,7 @@
+insert into "accounts" (
+  "username",
+  "hashedPassword"
+) values (
+  'dummyUser',
+  'asdhiuqewhd983y@#Y#$@#Y@_08rt39dfasdh2398y_@*y'
+)
--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,44 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+CREATE TABLE "public"."accounts" (
+	"userId" serial NOT NULL,
+	"username" TEXT NOT NULL UNIQUE,
+	"hashedPassword" TEXT NOT NULL,
+	"joinedAt" timestamp with time zone NOT NULL default now(),
+	CONSTRAINT "accounts_pk" PRIMARY KEY ("userId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public"."flashcards" (
+	"question" TEXT NOT NULL,
+	"answer" TEXT NOT NULL,
+	"userId" int NOT NULL,
+	"confidence" int NOT NULL,
+	"deckId" int NOT NULL
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public"."decks" (
+	"deckId" serial NOT NULL,
+	"deckName" TEXT NOT NULL,
+	"userId" int NOT NULL,
+	"deckConfidence" int NOT NULL,
+	CONSTRAINT "decks_pk" PRIMARY KEY ("deckId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+ALTER TABLE "flashcards" ADD CONSTRAINT "flashcards_fk0" FOREIGN KEY ("userId") REFERENCES "accounts"("userId");
+ALTER TABLE "flashcards" ADD CONSTRAINT "flashcards_fk1" FOREIGN KEY ("deckId") REFERENCES "decks"("deckId");
+
+ALTER TABLE "decks" ADD CONSTRAINT "decks_fk0" FOREIGN KEY ("userId") REFERENCES "accounts"("userId");
