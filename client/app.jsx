f70a0e11980c603604843d8105115cfd118e3aa9
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-11-07 17:49:28 +0000

    Create a component to render the modal and uses show prop to display or hide

--- client/app.jsx
+++ client/app.jsx
@@ -15,6 +15,7 @@ export default class App extends React.Component {
   }
 
   closeModal(event) {
+    event.preventDefault();
     this.setState({ show: false });
   }
 
--- client/pages/modal.jsx
+++ client/pages/modal.jsx
@@ -5,7 +5,27 @@ export default class Modal extends React.Component {
     if (!this.props.show) return null;
     return (
       <div className='modal-background'>
-        <h1>OH HEY I WORK</h1>
+        <div className="sticky-container">
+          <div className="sticky-outer">
+            <div className="sticky">
+              <svg width="0" height="0">
+                <defs>
+                  <clipPath id="stickyClip" clipPathUnits="objectBoundingBox">
+                    <path d="M 0 0 Q 0 0.69, 0.03 0.96 0.03 0.96, 1 0.96 Q 0.96 0.69, 0.96 0 0.96 0, 0 0"
+                      strokeLinejoin="round" strokeLinecap="square" />
+                  </clipPath>
+                </defs>
+              </svg>
+              <div className="sticky-content">
+                <form className="new-deck-form">
+                  <h1 className="sticky-header">Create New Deck</h1>
+                  <input className="sticky-input" type="text" placeholder="e.g. HTML, CSS, JavaScript"/>
+                  <button type="button" className="sticky-submit" onClick={this.props.closeModal}>Continue</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
