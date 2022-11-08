b0433bf72b0c0c72dc7c2ded3bf37694dd43aacf
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-11-08 01:02:33 +0000

    Added parse route and eventlistener to listen for hashchange

--- client/app.jsx
+++ client/app.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import Home from './pages/home';
 import Modal from './components/modal';
+import parseRoute from './lib/parse-route';
+import Decks from './components/decks';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = ({ show: false, decks: null, deckShowing: null });
+    this.state = ({
+      user: 1,
+      route: parseRoute(window.location.hash),
+      show: false,
+      decks: null,
+      deckShowing: null
+    });
     this.showModal = this.showModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
-    this.renderDecks = this.renderDecks.bind(this);
+    this.renderContent = this.renderContent.bind(this);
     this.showOptions = this.showOptions.bind(this);
     this.hideOptions = this.hideOptions.bind(this);
   }
@@ -33,49 +41,29 @@ export default class App extends React.Component {
   }
 
   componentDidMount() {
+    window.addEventListener('hashchange', () => {
+      this.setState({
+        route: parseRoute(window.location.hash)
+      });
+    });
     fetch('/api/decks')
       .then(res => res.json())
       .then(data => this.setState({ decks: data }))
       .catch(err => console.error(err));
   }
 
-  renderDecks() {
-    if (!this.state.decks) return;
-    const decks = this.state.decks.map(deck => {
-      const showPaper = Number(this.state.deckShowing) === deck.deckId ? 'is-showing' : '';
-      return (
-        <div key={deck.deckId} className='scene column-third'>
-          <div className='folder'>
-            <div className='folder-front folder-center' id={deck.deckId} onClick={this.showOptions}>
-              <h1 className='deck-text'>{deck.deckName}</h1>
-            </div>
-            <div className={`option-paper ${showPaper}`} onClick={this.hideOptions}>
-              <div className='stripes'>
-                <h1 className='deck-title'>{deck.deckName}</h1>
-                <section className='options-container'>
-                  <a href='/#add-card' className='add-card'>
-                    <i className="fa-solid fa-circle-plus" />
-                    Add Cards
-                  </a>
-                </section>
-              </div>
-            </div>
-            <div className='folder-tab' />
-            <div className='folder-back folder-center' />
-          </div>
-        </div>
-      );
-    });
-    return (
-      <div className='flex justify-center'>{decks}</div>
-    );
+  renderContent() {
+    const { path } = this.state.route;
+    if (path === '' || path === '#') {
+      return <Decks decks={this.state.decks} showing={this.state.deckShowing} showOptions={this.showOptions} hideOptions={this.hideOptions} />;
+    }
   }
 
   render() {
     return (
       <div>
         <Home showModal={this.showModal} />
-        { this.renderDecks() }
+        { this.renderContent() }
         <Modal show={this.state.show} closeModal={this.closeModal} />
       </div>
     );
---
+++ client/components/decks.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export default class Decks extends React.Component {
+  render() {
+    if (!this.props.decks) return;
+    const renderedDecks = this.props.decks.map(deck => {
+      const showPaper = Number(this.props.deckShowing) === deck.deckId ? 'is-showing' : '';
+      return (
+        <div key={deck.deckId} className='scene column-third'>
+          <div className='folder'>
+            <div className='folder-front folder-center' id={deck.deckId} onClick={this.props.showOptions}>
+              <h1 className='deck-text'>{deck.deckName}</h1>
+            </div>
+            <div className={`option-paper ${showPaper}`} onClick={this.props.hideOptions}>
+              <div className='stripes'>
+                <h1 className='deck-title'>{deck.deckName}</h1>
+                <section className='options-container'>
+                  <a href='/#add-card' className='add-card'>
+                    <i className="fa-solid fa-circle-plus" />
+                    Add Cards
+                  </a>
+                </section>
+              </div>
+            </div>
+            <div className='folder-tab' />
+            <div className='folder-back folder-center' />
+          </div>
+        </div>
+      );
+    });
+    return (
+      <div className='flex justify-center'>{renderedDecks}</div>
+    );
+  }
+}
