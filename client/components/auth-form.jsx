c9eacf1d9ecea1eec698540fdcf833df60f03ccf
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-11-16 00:22:27 +0000

    Styled sign up form for desktop and mobile

--- client/components/auth-form.jsx
+++ client/components/auth-form.jsx
@@ -5,16 +5,18 @@ export default class AuthForm extends React.Component {
     return (
       <div className="clipboard">
         <div className="clip t-center" />
-        <div className="page">
-          <form className="sign-in">
-            <h2 className="register-title">Greeting Sir</h2>
-            <label>Username
-              <input className="username" type="text" name="username" id="username" />
-            </label>
-            <label>Password
-              <input className="password" type="password" name="password" id="password" />
-            </label>
-            <button>Sign In</button>
+        <div className="page t-center">
+          <form className="auth flex wrap just-center">
+            <img className='placeholder-image col-2' src='/post-removebg-preview.png' />
+            <div className='form-section col-2'>
+              <label>Username
+                <input className="username" type="text" name="username" id="username" />
+              </label>
+              <label>Password
+                <input className="password" type="password" name="password" id="password" />
+              </label>
+              <button className='auth-button'>Sign Up</button>
+            </div>
           </form>
         </div>
       </div>
