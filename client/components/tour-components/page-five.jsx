c15b3b4892b643a6a5da6bf51b98126a2377242c
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-11-18 23:22:39 +0000

    completed page 5 tour

--- client/components/tour-components/page-five.jsx
+++ client/components/tour-components/page-five.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import AppContext from '../../lib/app-context';
 
 export default class PageFive extends React.Component {
   render() {
-    return <div>Page Five</div>;
+    return (
+      <section className='page-one-container'>
+        <section className='text-box'>
+          <h3 className='main-box-text'>
+            Let&apos;s go and make our first deck!
+          </h3>
+          <button
+          type='button'
+          className='get-started-button'
+          onClick={this.context.endTour}>
+            Get Started!
+          </button>
+        </section>
+        <img className='tour-image' src='/place.png' />
+      </section>
+    );
   }
 }
+
+PageFive.contextType = AppContext;
