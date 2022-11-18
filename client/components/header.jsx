2f39542704b1f90dcd3fc63172953e86738e5e68
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-11-18 17:55:37 +0000

    Prototype styling for adding images and deleted data from data.sql due to being inaccessible with user accounts implemented

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -5,14 +5,15 @@ export default class Header extends React.Component {
 
   render() {
     const show = this.context.user
-      ? 'sign-out-button'
+      ? ''
       : 'hidden';
     return (
       <header className='page-header'>
+        <img className={`idle-img ${show} hidden`} src='place.png' />
         <a href='#' className='page-title'>
           Page Title
         </a>
-        <button className={show} onClick={this.props.signOut}>
+        <button className={`sign-out-button ${show}`} onClick={this.props.signOut}>
           Sign Out
           <i className='fa-solid fa-right-from-bracket' />
         </button>
--- client/pages/notfound.jsx
+++ client/pages/notfound.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 export default function NotFound(props) {
   return (
-    <div className='flex wrap'>
+    <div className='flex wrap just-center'>
       <h3 className='not-found-text c-100'>
         Sorry, we could not find the page you were looking for!
       </h3>
-      <a className='return-home' href='#'>
+      <img className='not-found-img' src='place.png' />
+      <a className='return-home col-100' href='#'>
         Return Home
       </a>
     </div>
