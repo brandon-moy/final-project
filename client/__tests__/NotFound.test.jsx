abfbdd35bf4ea879264b82f1915631688d24d736
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-05-03 17:08:10 +0000

    Modified layout of NotFound test to describe test for page and run individual tests for each page component

--- client/__tests__/NotFound.test.jsx
+++ client/__tests__/NotFound.test.jsx
@@ -3,18 +3,27 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import NotFound from '../pages/notfound';
 
-test('Should render not found page elements', () => {
-  render(<NotFound />);
-  const apologyText = screen.getByTestId('not-found-text');
-  expect(apologyText).toBeInTheDocument();
-  expect(apologyText).toHaveTextContent('Sorry, we could not find the page you were looking for!');
+describe('Should render not found page elements', () => {
+  describe('Tests for individual elements on Not Found page', () => {
+    beforeEach(() => render(<NotFound />));
 
-  const notFoundImage = screen.getByAltText('Not Found Image');
-  expect(notFoundImage).toBeInTheDocument();
-  expect(notFoundImage.src).toContain('/images/shocked.webp');
+    test('Renders the not found test on the page', () => {
+      const apologyText = screen.getByTestId('not-found-text');
+      expect(apologyText).toBeInTheDocument();
+      expect(apologyText).toHaveTextContent('Sorry, we could not find the page you were looking for!');
+    });
 
-  const returnHomeLink = screen.getByTestId('not-found-return');
-  expect(returnHomeLink).toBeInTheDocument();
-  expect(returnHomeLink).toHaveTextContent('Return Home');
-  expect(returnHomeLink.href).toContain('#');
+    test('Renders the not found image on the page', () => {
+      const notFoundImage = screen.getByAltText('Not Found Image');
+      expect(notFoundImage).toBeInTheDocument();
+      expect(notFoundImage.src).toContain('/images/shocked.webp');
+    });
+
+    test('Renders the link to return to home page on screen', () => {
+      const returnHomeLink = screen.getByTestId('not-found-return');
+      expect(returnHomeLink).toBeInTheDocument();
+      expect(returnHomeLink).toHaveTextContent('Return Home');
+      expect(returnHomeLink.href).toContain('#');
+    });
+  });
 });
