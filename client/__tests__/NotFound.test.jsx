f027defb206533c3224ab268dd39d2c6f7b63f92
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-05-03 17:00:56 +0000

    Wrote some tests for NotFound page, tested and saving progress

---
+++ client/__tests__/NotFound.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import NotFound from '../pages/notfound';
+
+test('Should render not found page elements', () => {
+  render(<NotFound />);
+  const apologyText = screen.getByTestId('not-found-text');
+  expect(apologyText).toBeInTheDocument();
+  expect(apologyText).toHaveTextContent('Sorry, we could not find the page you were looking for!');
+
+  const notFoundImage = screen.getByAltText('Not Found Image');
+  expect(notFoundImage).toBeInTheDocument();
+  expect(notFoundImage.src).toContain('/images/shocked.webp');
+});
