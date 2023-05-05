5d5492be442ddefc0a3b4b7ac6353973ca1249a3
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-05-05 17:11:29 +0000

    Added additional tests for rendered elements on header when user is signed in

--- client/__tests__/Header.test.jsx
+++ client/__tests__/Header.test.jsx
@@ -5,19 +5,38 @@ import Header from '../components/header';
 import AppContext from '../lib/app-context';
 
 describe('Tests for the header component', () => {
-  beforeEach(() => {
-    const addItem = jest.fn({ user: true });
-    render(
-      <AppContext.Provider value={{ addItem }}>
-        <Header />
-      </AppContext.Provider>
-    );
-  });
-  test('It renders a link to the home page', () => {
-    const headerLink = screen.getByTestId('header-link');
+  describe('Tests for when a user is signed in', () => {
 
-    expect(headerLink).toBeInTheDocument();
-    expect(headerLink).toHaveTextContent('StudyBuddy');
-    expect(headerLink.href).toContain('#');
+    beforeEach(() => {
+      const addItem = jest.fn({ user: true });
+      render(
+        <AppContext.Provider value={{ addItem }}>
+          <Header />
+        </AppContext.Provider>
+      );
+    });
+
+    test('It renders a link to the home page', () => {
+      const headerLink = screen.getByTestId('header-link');
+
+      expect(headerLink).toBeInTheDocument();
+      expect(headerLink).toHaveTextContent('StudyBuddy');
+      expect(headerLink.href).toContain('#');
+    });
+
+    test('It renders an image in the corner when user is signed in', () => {
+      const headerImg = screen.getByTestId('header-img');
+
+      expect(headerImg).toBeInTheDocument();
+      expect(headerImg.src).toContain('/images/sit.webp');
+    });
+
+    test('It renders a sign-out button when the user is signed in', () => {
+      const signOutButton = screen.getByTestId('sign-out-button');
+
+      expect(signOutButton).toBeInTheDocument();
+      expect(signOutButton).toHaveTextContent('Sign Out');
+    });
+
   });
 });
