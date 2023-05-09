250a66ae1091ac94b97312a8b1518a8aabbfcf7b
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-05-09 19:17:34 +0000

    Created integration test for delete card modal form for warning message, confirm button, and cancel button

---
+++ client/__tests__/DeleteCard.test.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import DeleteForm from '../components/modal-components/deletecard';
+
+describe('Tests for a modal component', () => {
+  describe('Tests for the delete card modal form', () => {
+    beforeEach(() => {
+      render(
+        <DeleteForm />
+      );
+    });
+
+    test('It should render a warning header', () => {
+      const deleteCardHeader = screen.getByTestId('delete-card-header');
+
+      expect(deleteCardHeader).toBeInTheDocument();
+      expect(deleteCardHeader).toHaveTextContent('Are you sure you want to delete this card?');
+    });
+
+    test('The form should have cancel and confirm buttons', () => {
+      const cancelDelete = screen.getByTestId('cancel-delete');
+      const confirmDelete = screen.getByTestId('confirm-delete');
+
+      expect(cancelDelete).toBeInTheDocument();
+      expect(cancelDelete).toHaveTextContent('Cancel');
+      expect(confirmDelete).toBeInTheDocument();
+      expect(confirmDelete).toHaveTextContent('Continue');
+    });
+  });
+});
