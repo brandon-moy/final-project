391366cea6badff1ed953cd38dbe9ec80457d3c3
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-05-10 17:23:27 +0000

    Tests for rendered components on PageTwo of the tour

--- client/components/tour-components/page-two.jsx
+++ client/components/tour-components/page-two.jsx
@@ -4,13 +4,13 @@ export default class PageTwo extends React.Component {
   render() {
     return (
       <section className='page-two-container'>
-        <h1 className='new-deck-back-2' >
+        <h1 data-testid='new-folder' className='new-deck-back-2' >
           New Folder
         </h1>
-        <i className="fa-solid fa-angles-up arrow-2" />
+        <i data-testid='point-arrows' className="fa-solid fa-angles-up arrow-2" />
         <div className='flex just-f-end'>
           <section className='text-box-2'>
-            <h3 className='main-box-text'>
+            <h3 data-testid='page-two-instructions' className='main-box-text'>
               Great! First thing you &apos;ll need is a folder for your cards.
               This is where you&apos;ll create a new folder!
             </h3>
